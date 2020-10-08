ba55aaa183ae62dfab7a770e728d43f60dec9011
maiorem <maiorem00@gmail.com>
2020-10-08 18:06:45 +0900

    hash

---
+++ Level1/programmersSkillTest1_hash.py
@@ -0,0 +1,7 @@
+from collections import Counter 
+
+def solution(participant, completion):
+    answer = list((Counter(participant) - Counter(completion)).elements())[0]    
+    return answer
+    
+print(solution(["mark", "char"], ["mark"]))
