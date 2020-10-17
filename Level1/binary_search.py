0c310291ac4acae3808f75e106bf44a7fedea736
maiorem <maiorem00@gmail.com>
2020-10-17 15:15:05 +0900

    이진탐색 성공

--- Level1/binary_search.py
+++ Level1/binary_search.py
@@ -1,19 +1,16 @@
 def solution(L, x) :
-    answer=0
+    answer=-1
     low=0
     high=len(L)-1
-    if x in L :
-        while low<=high:
-            mid=(low+high)//2
-            if L[mid]==x :
-                answer=mid
-                break
-            elif L[mid]<x :
-                low=mid+1
-            else :
-                high=mid-1
-    else :
-        answer=-1
+    while low<=high:
+        mid=(low+high)//2
+        if L[mid]==x :
+            answer=mid
+            break
+        elif L[mid]<x :
+            low=mid+1
+        else :
+            high=mid-1
     return answer
 
 
